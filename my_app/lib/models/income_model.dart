8712c0cc937c4a0955c50de1ef53cdc5a92b988e
ishanfernando508 <ishanfernando508@gmail.com>
2026-01-07 10:20:09 +0530

    income and expense toggle

---
+++ my_app/lib/models/income_model.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+enum IncomeCategory {
+
+  freelance,
+  salary,
+  passive,
+  sales,
+
+}
+//category images
+final Map<IncomeCategory,String> incomeCategoryImages = {
+  IncomeCategory.freelance:"assets/images/freelance.png",
+  IncomeCategory.passive:"assets/images/car.png",
+  IncomeCategory.salary:"assets/images/health.png",
+  IncomeCategory.sales:"assets/images/salary.png",
+};
+
+
+//catogary colors
+final Map<IncomeCategory, Color> incomeCategoryColor = {
+  IncomeCategory.freelance : const Color(0XFFE57373),
+  IncomeCategory.passive : const Color(0xFF81C784),
+  IncomeCategory.sales: const Color(0XFF64B5F6),
+  IncomeCategory.salary : const Color(0XFFFFD54F),
+
+};
+
+class Income {
+  
+  final int id;
+  final String title;
+  final double amount;
+  final IncomeCategory category;
+  final DateTime date;
+  final DateTime time;
+  final String description;
+
+  Income({
+    required this.id,
+     required this.title,
+      required this.amount, 
+      required this.category,
+       required this.date, 
+       required this.time, 
+       required this.description,
+       });
+
+}
